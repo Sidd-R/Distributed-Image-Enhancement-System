2b9c421b0233b6b04452c8bca1ec8f9052da2880
Siddhant Rao <siddhant.rao@spit.ac.in>
2024-02-23 11:55:48 +0530

    image split and merge done

--- server/rpc_client.py
+++ server/rpc_client.py
@@ -3,7 +3,11 @@ import logging
 import grpc
 from protos import math_pb2_grpc
 from protos import math_pb2
+from protos import image_enhance_pb2_grpc
+from protos import image_enhance_pb2
 import threading
+from PIL import Image
+import io
 
 logging.basicConfig(
     filename="./logs/main_server.log",
@@ -12,61 +16,87 @@ logging.basicConfig(
 )
 
 
-def run(num1, num2):
-    with grpc.insecure_channel("localhost:50051") as channel:
-        stub = math_pb2_grpc.MathServiceStub(channel)
-        request = math_pb2.MathRequest(a=num1, b=num2)
+# def run(num1, num2):
+#     with grpc.insecure_channel("localhost:50051") as channel:
+#         stub = math_pb2_grpc.MathServiceStub(channel)
+#         request = math_pb2.MathRequest(a=num1, b=num2)
 
-        logging.info(f"rpc requests sent for {num1} and {num2}")
+#         logging.info(f"rpc requests sent for {num1} and {num2}")
         
-        result = []
+#         result = []
         
-        def thread_wrapper(func, request, name, result):
-            print(f"{name} thread started\n")
-            logging.info(f"{name} thread started")
-            response = func(request)
-            print(f"In {name} thread response is {response.result} for {request.a} and {request.b}\n")
-            logging.info(f"In {name} thread response is {response.result} for {request.a} and {request.b}")
-            # print(f"{name} thread completed\n")
-            result.append(response.result)
+#         def thread_wrapper(func, request, name, result):
+#             print(f"{name} thread started\n")
+#             logging.info(f"{name} thread started")
+#             response = func(request)
+#             print(f"In {name} thread response is {response.result} for {request.a} and {request.b}\n")
+#             logging.info(f"In {name} thread response is {response.result} for {request.a} and {request.b}")
+#             # print(f"{name} thread completed\n")
+#             result.append(response.result)
             
         
-        add_thread = threading.Thread(target=thread_wrapper, args=(stub.Addition, request, "add",result))
-        sub_thread = threading.Thread(target=thread_wrapper, args=(stub.Substraction, request, "sub",result))
-        mul_thread = threading.Thread(target=thread_wrapper, args=(stub.Multiplication, request, "mul",result))
-        div_thread = threading.Thread(target=thread_wrapper, args=(stub.Division, request, "div",result))
-        pow_thread = threading.Thread(target=thread_wrapper, args=(stub.Power, request, "pow",result))
+#         add_thread = threading.Thread(target=thread_wrapper, args=(stub.Addition, request, "add",result))
+#         sub_thread = threading.Thread(target=thread_wrapper, args=(stub.Substraction, request, "sub",result))
+#         mul_thread = threading.Thread(target=thread_wrapper, args=(stub.Multiplication, request, "mul",result))
+#         div_thread = threading.Thread(target=thread_wrapper, args=(stub.Division, request, "div",result))
+#         pow_thread = threading.Thread(target=thread_wrapper, args=(stub.Power, request, "pow",result))
 
-        # add_response = stub.Addition(request)
-        # sub_response = stub.Substraction(request)
-        # mul_response = stub.Multiplication(request)
-        # div_response = stub.Division(request)        
-        # pow_response = stub.Power(request)
+#         # add_response = stub.Addition(request)
+#         # sub_response = stub.Substraction(request)
+#         # mul_response = stub.Multiplication(request)
+#         # div_response = stub.Division(request)        
+#         # pow_response = stub.Power(request)
         
-        add_thread.start()
-        sub_thread.start()
-        mul_thread.start()
-        div_thread.start()
-        pow_thread.start()
+#         add_thread.start()
+#         sub_thread.start()
+#         mul_thread.start()
+#         div_thread.start()
+#         pow_thread.start()
         
-        add_thread.join()
-        sub_thread.join()
-        mul_thread.join()
-        div_thread.join()
-        pow_thread.join()
+#         add_thread.join()
+#         sub_thread.join()
+#         mul_thread.join()
+#         div_thread.join()
+#         pow_thread.join()
         
         
                 
 
-        logging.info(f"rpc requests completed for {num1} and {num2}")
-    # return [
-    #     add_response.result,
-    #     sub_response.result,
-    #     mul_response.result,
-    #     div_response.result,
-    #     pow_response.result,
-    # ]
+#         logging.info(f"rpc requests completed for {num1} and {num2}")
+#     # return [
+#     #     add_response.result,
+#     #     sub_response.result,
+#     #     mul_response.result,
+#     #     div_response.result,
+#     #     pow_response.result,
+#     # ]
     
-    # print(f"result is {result}")
+#     # print(f"result is {result}")
     
-    return result
+#     return result
+
+class RpcClient:
+    
+    def __init__(self) -> None:
+        self.lamport_timestamp = 0
+
+    def enhance_image(self):
+        image_path = "wumpus.jpeg"
+        with open(image_path, "rb") as f:
+            image_data = f.read()
+            
+            with grpc.insecure_channel("localhost:50051") as channel:
+                stub = image_enhance_pb2_grpc.ImageEnhancerStub(channel)
+                request = image_enhance_pb2.ImageRequest(id="1", image_data=image_data,lamport_timestamp=self.lamport_timestamp)
+                logging.info(f"image enhance request sent for {request.id}")
+                response = stub.EnhanceImage(request)
+                image = Image.open(io.BytesIO(response.image_data))
+                self.lamport_timestamp = max(self.lamport_timestamp,response.lamport_timestamp)
+                logging.info(f"image enhance response received for {request.id} at lamport timestamp {response.lamport_timestamp}")
+                
+                image.show()
+                # return response.image_path
+
+if __name__ == "__main__":
+    client = RpcClient()
+    client.enhance_image()
--- server/worker.py
+++ server/worker.py
@@ -9,9 +9,16 @@ import logging
 import grpc
 from protos import math_pb2
 from protos import math_pb2_grpc
+from protos import image_enhance_pb2
+from protos import image_enhance_pb2_grpc
+import io
+from PIL import Image
+import threading
 
 
 class RPCService(math_pb2_grpc.MathServiceServicer):
+    lamport_timestamp = 0
+    
     def Addition(self, request, context):
         logging.info(f"add request received for {request.a} and {request.b}")
         response = math_pb2.MathResponse(result=request.a + request.b)
@@ -41,6 +48,57 @@ class RPCService(math_pb2_grpc.MathServiceServicer):
         response = math_pb2.MathResponse(result=request.a**request.b)
         print(f"pow response is {response.result} for {request.a} and {request.b}")
         return response
+    
+class ImageEnhancerServicer(image_enhance_pb2_grpc.ImageEnhancerServicer):
+    lamport_timestamp = 0
+    
+    def process_part(self, part, processed_parts):
+        # Convert part to black and white
+        processed_part = part.convert('L')
+        processed_parts.append(processed_part)
+    
+    def EnhanceImage(self, request, context):
+        logging.info(f"image enhance request received for {request.id}")
+        image_bytes = request.image_data
+
+        # Decode the image using Pillow (PIL)
+        image = Image.open(io.BytesIO(image_bytes))
+        
+        num_parts = 4  # Example value, adjust as needed
+        width, height = image.size
+        part_height = height // num_parts
+        parts = []
+        for i in range(num_parts):
+            part = image.crop((0, i * part_height, width, (i + 1) * part_height))
+            parts.append(part)
+
+        # Process parts concurrently
+        processed_parts = []
+        threads = []
+        for part in parts:
+            thread = threading.Thread(target=self.process_part, args=(part, processed_parts))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        # Merge processed parts
+        merged_image = Image.new('RGB', (width, height))
+        for i, part in enumerate(processed_parts):
+            merged_image.paste(part, (0, i * part_height))
+
+        # Convert merged image to black and white
+        merged_image = merged_image.convert('L')
+
+        # Convert merged image back to bytes
+        with io.BytesIO() as output:
+            merged_image.save(output, format='JPEG')
+            merged_image_data = output.getvalue()
+                
+        self.lamport_timestamp = max(request.lamport_timestamp, self.lamport_timestamp) + 1
+        
+        return image_enhance_pb2.ImageResponse(image_data=merged_image_data,id=request.id, lamport_timestamp=self.lamport_timestamp)
 
 
 def serve():
@@ -48,8 +106,9 @@ def serve():
     
     id = sys.argv[2] if sys.argv[2:] else "1"
 
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    math_pb2_grpc.add_MathServiceServicer_to_server(RPCService(), server)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    # math_pb2_grpc.add_MathServiceServicer_to_server(RPCService(), server)
+    image_enhance_pb2_grpc.add_ImageEnhancerServicer_to_server(ImageEnhancerServicer(), server)
     server.add_insecure_port("[::]:" + PORT)
     server.start()
     print("Worker server is running on port: " + PORT)
