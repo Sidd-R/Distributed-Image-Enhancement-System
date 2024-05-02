6b39de2d6d472829c4baf64b5df5d0d4a4badde9
Rathod _ <8055shubhamrathod@gmail.com>
2024-05-03 03:31:34 +0530

    some changes

--- server/image_utils/image_filter.py
+++ server/image_utils/image_filter.py
@@ -1,9 +1,24 @@
 # implement image filter logic
+import cv2
+import numpy as np
+from PIL import Image
 
 class ImageFilter:
     def __init__(self):
         pass
 
-    def filter(self, image):
+    def filter(self, image, choice):
         # implement image filter logic
-        return image
+        image = np.array(image)
+        if choice == 0:
+            kernel = np.ones((5, 5), np.uint8) 
+            image = cv2.erode(image, kernel, iterations=1)
+        elif choice == 1:
+            kernel = np.ones((5, 5), np.uint8) 
+            image = cv2.dilate(image, kernel, iterations=1)
+        elif choice == 2:
+            kernel = np.ones((5, 5), np.uint8)
+            image = cv2.morphologyEx(image, cv2.MORPH_GRADIENT, kernel)
+        elif choice == 3:
+            image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 15) 
+        return Image.fromarray(image)
